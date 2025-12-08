4b2960ea29edadd402a6dce9447009f04f77446d
mirazahmeed <mirazahmed0619@gmail.com>
2025-12-09 00:05:53 +0600

    feat: Add register form submission handler with input field attributes and default values.

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import { Link } from "react-router";
 
 const Register = () => {
+	const handleRegister = (e) => {
+		e.preventDefault();
+		const form = e.target;
+		const name = form.name.value;
+		const photoURL = form.photoURL.value;
+		const email = form.email.value;
+		const password = form.password.value;
+		console.log(name, photoURL, email, password);
+	};
 	return (
 		<div>
 			<div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto">
@@ -9,11 +18,13 @@ const Register = () => {
 					Register Your account
 				</h2>
 				<div className="card-body">
-					<form className="fieldset">
+					<form onSubmit={handleRegister} className="fieldset">
 						{/* Name */}
 						<label className="label">Name</label>
 						<input
 							type="text"
+							name="name"
+							defaultValue={"Miraz"}
 							className="input rounded-none"
 							placeholder="Enter your name"
 						/>
@@ -21,6 +32,10 @@ const Register = () => {
 						<label className="label">PhotoURL</label>
 						<input
 							type="text"
+							name="photoURL"
+							defaultValue={
+								"https://i.ibb.co.com/B2CQ44N5/alrick-gillard-0m-N84vxig-L0-unsplash.jpg"
+							}
 							className="input rounded-none"
 							placeholder="Enter your photo URL"
 						/>
@@ -28,6 +43,8 @@ const Register = () => {
 						<label className="label">Email</label>
 						<input
 							type="email"
+							name="email"
+							defaultValue={"miraz07ahmed@gmail.com"}
 							className="input rounded-none"
 							placeholder="Enter your email"
 						/>
@@ -35,13 +52,17 @@ const Register = () => {
 						<label className="label">Password</label>
 						<input
 							type="password"
+							name="password"
+							defaultValue={"123456"}
 							className="input rounded-none"
 							placeholder="Enter your password"
 						/>
 						<div>
 							<a className="link link-hover">Forgot password?</a>
 						</div>
-						<button className="btn btn-neutral mt-4 rounded-none">
+						<button
+							type="submit"
+							className="btn btn-neutral mt-4 rounded-none">
 							Register
 						</button>
 						<Link to="/auth/login" className="link link-hover pt-3">
